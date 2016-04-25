a43fd8109d7c865c493bf7da4103fc80b746cfd2
SerzhNS <mhm.5571@gmail.com>
2016-04-26 00:57:07 +0300

    Корректировка задачи 02_06

--- task_02_06.rb
+++ task_02_06.rb
@@ -20,19 +20,21 @@ g_un_price = g_un_pr[1].to_f
 puts "Введите количество приобретенного товара"
 g_vol = gets().chomp.to_f
 
-purchase_bug.merge!({g_name => {g_unit => g_un_price, "количество #{g_vol}" => (g_vol*g_un_price)}})
+purchase_bug.merge!({g_name => {"цена за  1 #{g_unit}" => g_un_price, "стоимость #{g_vol}" => (g_vol*g_un_price)}})
 
 end
 
 # Печать и расчет стоимости покупок
 total_sum = 0
 
-p(purchase_bug) # Temp
+# p(purchase_bug) # Temp
+
+puts "==> Корзина покупок включает:"
 
 purchase_bug.each_key do |key| 
-  #print ("#{key}\t#{purchase_bug[key]}\n")
+  print ("#{key}\t#{purchase_bug[key]}\n")
   arr = purchase_bug[key].to_a
   total_sum += arr[1][1]  
 end
 
-puts "==> Сумма покупок равна #{total_sum}"
+puts "==> Сумма покупок равна #{total_sum.round(2)}"
